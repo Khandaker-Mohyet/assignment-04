79e66524d1ba2b9e7e823756891933921b19f6ba
Khandaker Mohyet <mohyet0007@gmail.com>
2024-09-12 02:01:23 +0600

    Some corriction

--- index.js
+++ index.js
@@ -109,10 +109,10 @@ function  waitingTime(waitingTimes , serialNumber) {
   }
 }
 
-console.log(waitingTime([ 3, 5, 7, 11, 6 ], 10))
-console.log(waitingTime([13, 2], 6))
-console.log(waitingTime([13, 2, 6, 7, 10], 6))
-console.log(waitingTime([6], 4))
-console.log(waitingTime(7 , 10))
-console.log(waitingTime("[6,2]", 9))
-console.log(waitingTime([7, 8, 3, 4, 5], "9"))
+// console.log(waitingTime([ 3, 5, 7, 11, 6 ], 10))
+// console.log(waitingTime([13, 2], 6))
+// console.log(waitingTime([13, 2, 6, 7, 10], 6))
+// console.log(waitingTime([6], 4))
+// console.log(waitingTime(7 , 10))
+// console.log(waitingTime("[6,2]", 9))
+// console.log(waitingTime([7, 8, 3, 4, 5], "9"))
